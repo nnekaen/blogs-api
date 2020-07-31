ce138654901803e8cb014134b423583f1ceb8066
Nneka Edozien <nnekaen12@yahoo.com>
2020-07-31 12:36:31 +0000

    commit

--- wp-content/themes/blogapi/single.php
+++ wp-content/themes/blogapi/single.php
@@ -16,8 +16,14 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
-
-			    the_post_navigation();
+             ?> <div class="csbtns">Share This!
+			<a class="csbtns_reddit_share" href="http://reddit.com/submit?url=https://protonmail.com/blog/privacy-contact-tracing-apps/&amp;title=What you need to know about contact tracing apps and privacy" target="_blank" aria-label=" (opens in a new tab)"><i class="csbtns fa fa-reddit" ></i></a>
+		  <a class="csbtns_facebook_share" href="http://www.facebook.com/sharer.php?u=https://protonmail.com/blog/privacy-contact-tracing-apps/" target="_blank" aria-label=" (opens in a new tab)"><i class="csbtns fa fa-facebook" ></i></a>
+		  <a class="csbtns_email_share" href="mailto:?Subject=What you need to know about contact tracing apps and privacy&amp;Body=%20https://protonmail.com/blog/privacy-contact-tracing-apps/" aria-label=""><i class="csbtns fa fa-envelope" ></i></a>
+		  <a class="csbtns_twitter_share" href="http://twitter.com/share?url=https://protonmail.com/blog/privacy-contact-tracing-apps/&amp;text=What+you+need+to+know+about+contact+tracing+apps+and+privacy+" target="_blank" aria-label=" (opens in a new tab)"><i class="csbtns fa fa-twitter" ></i></a>
+			</div>
+			    <?php
+			the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -26,7 +32,10 @@ get_header(); ?>
 
 		endwhile; // End of the loop.
 		?>
-
+		<h4>
+			Share
+			</h4>
+      
 		</main><!-- #main -->
 	</section><!-- #primary -->
   
